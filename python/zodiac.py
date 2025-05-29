f3e4481b94913300774572427e5d16f4effb7cfd
Pat Tul <patchatulyakul@gmail.com>
2025-05-29 17:46:08 -0400

    add zodiac.py

--- python/zodiac.py
+++ python/zodiac.py
@@ -1,1 +1,32 @@
 input = input("Input your birthday(mm/dd/yyyy): ")
+birthday = input.split("/")
+month = birthday[0]
+date = birthday[1]
+year = birthday[2]
+
+if  (month == "12" and date >= "22") or (month == "01" and date <= "19"):
+    print("♑︎︎ Capricorn")
+elif (month == "01" and date >= "20") or (month == "02" and date <= "18"):
+    print("♒︎︎ Aquarius")
+elif (month == "02" and date >= "19") or (month == "03" and date <= "20"):
+    print("♓︎︎ Pisces")    
+elif (month == "03" and date >= "21") or (month == "04" and date <= "19"):
+    print("♈︎︎ Aries")
+elif (month == "04" and date >= "20") or (month == "05" and date <= "20"):
+    print("♉︎︎ Taurus")
+elif (month == "05" and date >= "21") or (month == "06" and date <= "20"):
+    print("♊︎︎ Gemini")
+elif (month == "06" and date >= "21") or (month == "07" and date <= "22"):
+    print("♋︎︎ Cancer")
+elif (month == "07" and date >= "23") or (month == "08" and date <= "22"):
+    print("♌︎︎ Leo")
+elif (month == "08" and date >= "23") or (month == "09" and date <= "22"):
+    print("♍︎︎ Virgo")
+elif (month == "09" and date >= "23") or (month == "10" and date <= "22"):
+    print("♎︎︎ Libra")
+elif (month == "10" and date >= "23") or (month == "11" and date <= "21"):
+    print("♏︎︎ Scorpio")
+elif (month == "11" and date >= "22") or (month == "12" and date <= "21"):
+    print("♐︎︎ Sagittarius")
+else:
+    print("Input again")
