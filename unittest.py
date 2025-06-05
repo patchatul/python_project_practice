7d93736e7489e81420113dfca8b5ac2dd8fc1fa6
Pat Tul <patchatulyakul@gmail.com>
2025-06-04 20:01:21 -0400

    add unittest

---
+++ unittest.py
@@ -0,0 +1,10 @@
+import unittest
+def add(a,b):
+    return a+b
+class TestAddition(unittest.TestCase):
+    def test_add(self):
+        result = add(3,4)
+        expected_result =7
+        self.assertEqual(result, expected_result)
+if __name__ == '__main__':
+    unittest.main()
